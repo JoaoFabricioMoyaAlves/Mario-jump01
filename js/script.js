9842b9af2c06af609095eec7638c5539b16c2d6c
Joao Fabrício Moya Alves <JoaoFabricioMoyaAlves@users.noreply.github.com>
2023-12-25 09:50:03 -0300

    Projeto Mario Jump

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const gameover = document.querySelector('.game-over')
+const jump = () => {
+  mario.classList.add('jump');
+
+  setTimeout(()=>{
+    mario.classList.remove('jump');
+
+  },800);
+
+}
+
+const loop = setInterval(() => {
+         
+  console.log('loop')
+
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+   
+    console.log(marioPosition);
+    
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+      pipe.style.animation = 'none';
+      pipe.style.left=`${pipePosition}px`;
+
+      mario.style.animation = 'none';
+      mario.style.bottom=`${marioPosition}px`;
+
+      mario.src = 'imagens/game-over.png';
+      mario.style.width = '75px';
+      mario.style.marginLeft = '50px';
+
+      gameover.style.display = 'block';
+    
+     clearInterval(loop);
+   
+    }
+
+},10)
+
+function recarregarPagina() {
+ 
+  location.reload();
+}
+
+
+
+document.addEventListener('keydown',jump);
